cf45ae985b289b0d3762fc6db59932f29c4551f4
Dylan Ryan <drryan@ncsu.edu>
2015-08-24 00:09:10 -0400

    Initial commit

---
+++ edd-mixpanel.php
@@ -0,0 +1,388 @@
+<?php
+/**
+ * Plugin Name:     Easy Digital Downloads - Mixpanel Lite
+ * Plugin URI:      http://eddmixpanel.com/
+ * Description:     Easily integrate Mixpanel analytics with Easy Digital Downloads.
+ * Version:         1.0.0
+ * Author:          Dylan Ryan
+ * Author URI:      http://dylanryan.co
+ * Text Domain:     edd-mixpanel
+ *
+ * @package         EDD\EDD_Mixpanel
+ * @author          Dylan Ryan
+ * @copyright       Copyright (c) Dylan Ryan
+ */
+
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) exit;
+
+if( !class_exists( 'EDD_Mixpanel' ) ) {
+
+    /**
+     * Main EDD_Mixpanel class
+     *
+     * @since 1.0.0
+     */
+    class EDD_Mixpanel {
+
+        /**
+         * @var bool
+         * @since 1.0.0
+         */
+        private $track = false;
+
+        /**
+         * @var     MixPanel
+         * @since   1.0.0
+         */
+        private $mixpanel;
+
+        /**
+         * @var         EDD_Mixpanel $instance The one true EDD_Mixpanel
+         * @since       1.0.0
+         */
+        private static $instance;
+
+
+        /**
+         * Get active instance
+         *
+         * @access      public
+         * @since       1.0.0
+         * @return      object self::$instance The one true EDD_Mixpanel
+         */
+        public static function instance() {
+            if( !self::$instance ) {
+                self::$instance = new EDD_Mixpanel();
+                self::$instance->setup_constants();
+                self::$instance->includes();
+                self::$instance->load_textdomain();
+                self::$instance->hooks();
+            }
+
+            return self::$instance;
+        }
+
+
+        /**
+         * Setup plugin constants
+         *
+         * @access      private
+         * @since       1.0.0
+         * @return      void
+         */
+        private function setup_constants() {
+            // Plugin version
+            define( 'EDD_MIXPANEL_VER', '1.0.0' );
+
+            // Plugin path
+            define( 'EDD_MIXPANEL_DIR', plugin_dir_path( __FILE__ ) );
+
+            // Plugin URL
+            define( 'EDD_MIXPANEL_URL', plugin_dir_url( __FILE__ ) );
+        }
+
+
+        /**
+         * Include necessary files
+         *
+         * @access      private
+         * @since       1.0.0
+         * @return      void
+         */
+        private function includes() {
+            // Mixpanel PHP Library
+            require_once EDD_MIXPANEL_DIR . 'vendor/mixpanel/mixpanel-php/lib/Mixpanel.php';
+
+        }
+
+
+        /**
+         * Run action and filter hooks
+         *
+         * @access      private
+         * @since       1.0.0
+         * @return      void
+         */
+        private function hooks() {
+            global $edd_options;
+
+            // Register settings
+            add_filter( 'edd_settings_extensions', array( $this, 'settings' ), 1 );
+
+			// Track items added to the cart
+            if ( $edd_options['edd_mixpanel_track_added_to_cart'] == true ) {
+                add_action( 'edd_post_add_to_cart', array( $this, 'track_added_to_cart' ) );
+            }
+
+            // Track completed purchases
+            if ( $edd_options['edd_mixpanel_track_completed_purchases'] == true ) {
+                add_action( 'edd_update_payment_status', array( $this, 'track_completed_purchase' ), 100, 3 );
+            }
+        }
+
+
+        /**
+         * Internationalization
+         *
+         * @access      public
+         * @since       1.0.0
+         * @return      void
+         */
+        public function load_textdomain() {
+            // Set filter for language directory
+            $lang_dir = EDD_MIXPANEL_DIR . '/languages/';
+            $lang_dir = apply_filters( 'edd_mixpanel_languages_directory', $lang_dir );
+
+            // Traditional WordPress plugin locale filter
+            $locale = apply_filters( 'plugin_locale', get_locale(), 'edd-mixpanel' );
+            $mofile = sprintf( '%1$s-%2$s.mo', 'edd-mixpanel', $locale );
+
+            // Setup paths to current locale file
+            $mofile_local   = $lang_dir . $mofile;
+            $mofile_global  = WP_LANG_DIR . '/edd-mixpanel/' . $mofile;
+
+            if( file_exists( $mofile_global ) ) {
+                // Look in global /wp-content/languages/edd-mixpanel/ folder
+                load_textdomain( 'edd-mixpanel', $mofile_global );
+            } elseif( file_exists( $mofile_local ) ) {
+                // Look in local /wp-content/plugins/edd-mixpanel/languages/ folder
+                load_textdomain( 'edd-mixpanel', $mofile_local );
+            } else {
+                // Load the default language files
+                load_plugin_textdomain( 'edd-mixpanel', false, $lang_dir );
+            }
+        }
+
+
+        /**
+         * Add settings
+         *
+         * @access      public
+         * @since       1.0.0
+         * @param       array $settings The existing EDD settings array
+         * @return      array The modified EDD settings array
+         */
+        public function settings( $settings ) {
+            $new_settings = array(
+                array(
+                    'id'    => 'edd_mixpanel_settings',
+                    'name'  => '<strong>' . __( 'Mixpanel Settings', 'edd-mixpanel' ) . '</strong>',
+                    'desc'  => __( 'Configure Mixpanel Settings', 'edd-mixpanel' ),
+                    'type'  => 'header',
+                ),
+                array(      // API Key
+                    'id'    => 'edd_mixpanel_api_key',
+                    'name'  => __( 'Project Token', 'edd-mixpanel' ),
+                    'desc'  => __( 'Enter the Token for the Mixpanel Project you want to track data for.', 'edd-mixpanel' ),
+                    'type'  => 'text',
+                    'size'  => 'regular'
+                ),
+                array(      // Added to cart
+                    'id'    => 'edd_mixpanel_track_added_to_cart',
+                    'name'  => __( 'Track "Added To Cart"?', 'edd-mixpanel' ),
+                    'desc'  => __( 'Check this box to track users that added items to their cart.', 'edd-mixpanel' ),
+                    'type'  => 'checkbox'
+                ),
+                array(      // Added to cart label
+                    'id'    => 'edd_mixpanel_track_added_to_cart_label',
+                    'name'  => __( '"Added To Cart" Label', 'edd-mixpanel' ),
+                    'desc'  => __( 'Enter the label that identifies this action in Mixpanel. Default: EDD Added To Cart', 'edd-mixpanel' ),
+                    'type'  => 'text',
+                    'size'  => 'medium',
+                    'std'   => __( 'EDD Added To Cart', 'edd-mixpanel' )
+                ),
+                array(      // Completed purchases
+                    'id'    => 'edd_mixpanel_track_completed_purchases',
+                    'name'  => __( 'Track "Completed Purchases"?', 'edd-mixpanel' ),
+                    'desc'  => __( 'Check this box to track completed purchases.', 'edd-mixpanel' ),
+                    'type'  => 'checkbox'
+                ),
+                array(      // Completed purchases label
+                    'id'    => 'edd_mixpanel_track_completed_purchases_label',
+                    'name'  => __( '"Completed Purchases" Label', 'edd-mixpanel' ),
+                    'desc'  => __( 'Enter the label that identifies this action in Mixpanel. Default: EDD Completed Purchase', 'edd-mixpanel' ),
+                    'type'  => 'text',
+                    'size'  => 'medium',
+                    'std'   => __( 'EDD Completed Purchase', 'edd-mixpanel' )
+                ),
+            );
+
+            return array_merge( $settings, $new_settings );
+        }
+
+
+        /**
+         * Helper function to set Mixpanel token.
+         *
+         * @access      private
+         * @since       1.0.0
+         */
+        private function set_token() {
+            global $edd_options;
+
+            // Grab the API Key
+            $token = isset( $edd_options['edd_mixpanel_api_key'] ) ? trim( $edd_options['edd_mixpanel_api_key'] ) : false;
+
+            // Setup the Mixpanel instance
+            $this->mixpanel = Mixpanel::getInstance( $token );
+
+            // Start a session
+            if ( ! session_id() ) {
+                session_start();
+            }
+
+            // If we don't have a key, bail
+            if( ! empty( $token ) )
+                $this->track = true;
+        }
+
+
+        /**
+         * Track items added to a user's cart.
+         *
+         * @since   1.0.0
+         *
+         * @param   int     $download_id    Download ID number
+         * @param   array   $options        Optional parameters, used for defining variable prices
+         */
+        public function track_added_to_cart( $download_id = 0, $options = array() ) {
+	        global $edd_options;
+
+            // Store tracked event properties
+            $event_props = array();
+
+            // Setup Mixpanel instance
+            $this->set_token();
+
+	        // If we failed, bail
+            if( ! $this->track )
+                return;
+
+            // If we are logged in, grab IP & current user data
+            if( is_user_logged_in() ) {
+                $person_props['ip']         = edd_get_ip();
+                $event_props['distinct_id'] = get_current_user_id();
+                $this->mixpanel->people->set( get_current_user_id(), $person_props );
+            }
+
+	        // Send the product, session, and user data
+            $event_props['ip']           = edd_get_ip();
+            $event_props['session_id']   = session_id();
+            $event_props['product_name'] = get_the_title( $download_id );
+            $event_props['product_price']= edd_get_cart_item_price( $download_id, $options );
+
+	        // If subscription (Restrict Content Pro) data exists, send it too
+            if( function_exists( 'rcp_get_subscription' ) && is_user_logged_in() ) {
+                $event_props['subscription'] = rcp_get_subscription( get_current_user_id() );
+            }
+
+            // Prepare the label to send to Mixpanel
+            if ( isset( $edd_options['edd_mixpanel_track_added_to_cart_label'] ) ) {
+                // Grab the label from EDD Options
+                $label = trim( $edd_options['edd_mixpanel_track_added_to_cart_label'] );
+            } else {
+                // If we don't have one set, then default
+                $label = 'EDD Added to Cart';
+            }
+
+            // Log event in Mixpanel
+            $this->mixpanel->track( $label, $event_props );
+        }
+
+
+        /**
+         * Track when a user completes a purchase.
+         *
+         * @param   int     $payment_id Payment ID
+         * @param   string  $new_status New payment status
+         * @param   string  $old_status Old payment status
+         */
+        public function track_completed_purchase( $payment_id, $new_status, $old_status ) {
+            // Store tracked event, person, and product properties
+            $event_props    = array();
+            $person_props   = array();
+            $products       = array();
+
+            // Setup Mixpanel instance
+            $this->set_token();
+
+            // If we failed, bail
+            if( ! $this->track )
+                return;
+
+            // Make sure that payments are only completed once
+            if ( $old_status == 'publish' || $old_status == 'complete' )
+                return;
+
+            // Make sure the payment completion is only processed when new status is complete
+            if ( $new_status != 'publish' && $new_status != 'complete' )
+                return;
+
+            $user_info = edd_get_payment_meta_user_info( $payment_id );
+            $user_id   = edd_get_payment_user_id( $payment_id );
+            $downloads = edd_get_payment_meta_cart_details( $payment_id );
+            $amount    = edd_get_payment_amount( $payment_id );
+            if( $user_id <= 0 ) {
+                $distinct = $user_info['email'];
+            } else {
+                $distinct = $user_id;
+            }
+            $person_props['$first_name']   = $user_info['first_name'];
+            $person_props['$last_name']    = $user_info['last_name'];
+            $person_props['$email']        = $user_info['email'];
+            $person_props['ip']            = edd_get_ip();
+            $this->mixpanel->people->set( $distinct, $person_props );
+
+            $event_props['distinct_id']   = $distinct;
+            $event_props['amount']        = $amount;
+            $event_props['session_id']    = session_id();
+            $event_props['purchase_date'] = strtotime( get_post_field( 'post_date', $payment_id ) );
+
+            foreach( $downloads as $download ) {
+                $products[] = get_the_title( $download['id'] );
+            }
+
+            $event_props['products'] = implode( ', ', $products );
+
+            // Prepare the label to send to Mixpanel
+            if ( isset( $edd_options['edd_mixpanel_track_completed_purchases_label'] ) ) {
+                // Grab the label from EDD Options
+                $label = trim( $edd_options['edd_mixpanel_track_completed_purchases_label'] );
+            } else {
+                // If we don't have one set, then default
+                $label = 'EDD Completed Purchase';
+            }
+
+            // Log event in Mixpanel
+            $this->mixpanel->track( $label, $event_props );
+
+            // Log charge to customer in Mixpanel
+            $this->mixpanel->people->trackCharge( $distinct, $amount );
+        }
+    }
+} // End if class_exists check
+
+
+/**
+ * The main function responsible for returning the one true EDD_Mixpanel
+ * instance to functions everywhere
+ *
+ * @since       1.0.0
+ * @return      \EDD_Mixpanel The one true EDD_Mixpanel
+ */
+function edd_mixpanel_load() {
+    if( ! class_exists( 'Easy_Digital_Downloads' ) ) {
+        if( ! class_exists( 'EDD_Extension_Activation' ) ) {
+            require_once 'includes/class.extension-activation.php';
+        }
+
+        $activation = new EDD_Extension_Activation( plugin_dir_path( __FILE__ ), basename( __FILE__ ) );
+        $activation = $activation->run();
+    } else {
+        return EDD_Mixpanel::instance();
+    }
+    return null;
+}
+add_action( 'plugins_loaded', 'edd_mixpanel_load' );
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== EDD Mixpanel ===
+Contributors: irkanu
+Donate link: https://eddmixpanel.com/
+Tags: edd, easy digital downloads, mixpanel, mixpanelphp, analytics, free, wordpress
+Requires at least: 3.0.0
+Tested up to: 4.3.0
+Stable tag: 1.0.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Connect Mixpanel with Easy Digital Downloads: Visualize product revenue, customer data, conversion rates, and more.
+
+== Description ==
+= Overview =
+The <strong>EDD Mixpanel</strong> plugin allows you to quickly and easily connect your Easy Digital Downloads store to the most powerful analytical software. Mixpanel makes it easy to track customers, revenue, funnels, and more. The platform is incredibly easy-to-use. Our premium version is currently in alpha testing & all information available here may or may not make it into production. If you are interested in beta testing, when the time comes, then please [get in touch](https://eddmixpanel.com/contact/).
+
+> <strong>Priority Support</strong><br>
+> We do not provide support for EDD Mixpanel on the WordPress.org forums. Priority email support is available to those that purchase the [Premium EDD Mixpanel plugin](https://eddmixpanel.com/) only.
+> The premium version allows you to track up to 24 events.
+>
+> We will be providing short video tutorials to help you get the most out of Mixpanel.
+
+= Built for Mixpanel =
+This Easy Digital Downloads extension was developed to integrate with [Mixpanel](https://mixpanel.com), one of the most granular analytics tool available. If you haven't already, you'll need at least a [free account](https://mixpanel.com/register/) to get started.
+
+= Turn Insight Into Action =
+We'll show you how to track your revenue overtime and find which products are performing well and which aren't. Mixpanel's granularity allows you to segment data by product, category, location, and much more!
+
+= Visualize The Data =
+Stop waisting your time looking at numbers on a screen and easily generate graphs based on your sales funnel. This will allow you to find out where your checkout process is doing well, or not so well.
+
+= Real-Time Analytics =
+Who doesn't love real-time analytics? Instantly see how your customers are interacting with your Easy Digital Downloads store.
+
+= Choose What Gets Tracked =
+The free version of EDD Mixpanel only allows 2 events to be tracked; however, the premium version has up to 24 available actions to track.
+
+= Disclosure =
+I, the plugin author, am not affiliated with [Mixpanel](https://mixpanel.com/), nor [Easy Digital Downloads](https://easydigitaldownloads.com/). All trademarks or registered trademarks are property of their respective owners. I enjoy building products that help people connect their revenue with the data.
+
+== Installation ==
+
+= Automatic Installation =
+
+The easiest way to install this plugin is automatically through WordPress because it will handle all of the file transferring.
+
+= Manual Installation =
+
+The manual installation method involves downloading the plugin and uploading it to your server via your favorite FTP application. The WordPress codex contains [instructions on how to do this here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+== Frequently Asked Questions ==
+
+= How do I gain more data points? =
+
+It's easy! Become a [Mixpanel Partner](https://mixpanel.com/free/) by adding their badge.
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release
+
+== Upgrade Notice ==
